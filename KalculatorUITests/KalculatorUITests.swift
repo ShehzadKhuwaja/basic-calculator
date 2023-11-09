6b7229afe8fefd507becab533fbbfdb61939b83b
shehzad <shehzadkhuwaja@gmail.com>
2023-11-09 23:17:57 +0500

    all test cases including UI test Cases are now running Successfully

--- KalculatorUITests/KalculatorUITests.swift
+++ KalculatorUITests/KalculatorUITests.swift
@@ -10,13 +10,13 @@ import XCTest
 
 final class KalculatorUITests: XCTestCase {
 
-    var app:XCUIApplication?
+    let app = XCUIApplication()
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         // UI tests must launch the application that they test
+        super.setUp()
         
-        let app = XCUIApplication()
         app.launch()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
@@ -30,36 +30,53 @@ final class KalculatorUITests: XCTestCase {
     }
     
     func testButtonsExist() throws {
+        //XCTAssert(app.staticTexts["result"].exists)
+        // test if all number buttons are on screen
         for i in 0...9 {
-            XCTAssertNotNil(app?.buttons["\(i)"], "Number button \(i) is missing.")
+            XCTAssertNotNil(app.buttons["\(i)"], "Number button \(i) is missing.")
         }
-    
+        
+        // test all operation buttons are on screen
         let operators = ["+", "-", "*", "/", "="]
         for operatorSymbol in operators {
-            XCTAssertNotNil(app?.buttons[operatorSymbol], "Operator button '\(operatorSymbol)' is missing.")
+            XCTAssertNotNil(app.buttons[operatorSymbol], "Operator button '\(operatorSymbol)' is missing.")
         }
+        
     }
     
     func testOutputDisplay() {
         // Input numbers and operators
         
-        app?.buttons["2"].tap()
-        app?.buttons["+"].tap()
-        app?.buttons["2"].tap()
-        app?.buttons["="].tap()
+        app.buttons["2"].tap()
+        app.buttons["+"].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        
         
         // Verify the result is displayed
-        let resultLabel = app?.staticTexts["result"]
-        XCTAssertEqual(resultLabel?.label, "9", "Result is not displayed as expected")
-
+        let resultLabel = app.staticTexts["result"]
+        //print(resultLabel.label)
+        
+    
+        XCTAssertEqual(resultLabel.label, "4", "Result is not displayed as expected")
+    
         // Input invalid input to trigger an error
-        app?.buttons["/"].tap()
-        app?.buttons["0"].tap()
-        app?.buttons["="].tap()
+        app.buttons["/"].tap()
+        app.buttons["0"].tap()
+        app.buttons["="].tap()
 
         // Verify the error is displayed
-        let errorLabel = app?.staticTexts["result"]
-        XCTAssertEqual(errorLabel?.label, "Err", "Result is not displayed as expected")
+        let errorLabel = app.staticTexts["result"]
+        XCTAssertEqual(errorLabel.label, "Err", "Result is not displayed as expected")
+    
     }
+    
+    func testClearButton() {
+        app.buttons["1"].tap()
+        app.buttons["C"].tap()
 
+        let result = app.staticTexts["result"]
+        XCTAssertEqual(result.label, "0")
+    }
+    
 }
